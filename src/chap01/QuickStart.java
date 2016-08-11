9288e6da4d45f566144d0a635f55e0badc117fd6
Administrator <617317499@qq.com>
2016-08-11 22:34:29 +0800

    This is the first version.

---
+++ src/chap01/QuickStart.java
@@ -0,0 +1,68 @@
+package chap01;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HeaderIterator;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+public class QuickStart {
+
+	public static void main(String[] args) throws Exception {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpGet httpGet = new HttpGet("http://httpbin.org/get");
+			CloseableHttpResponse getResp = httpclient.execute(httpGet);
+			// The underlying HTTP connection is still held by the response
+			// object to allow the response content to be streamed directly from the network socket. 
+			// In order to ensure correct deallocation of system resources
+			// the user MUST call CloseableHttpResponse#close() from a finally clause.
+			// Please note that if response content is not fully consumed the
+			// underlying connection cannot be safely re-used and will be shut down and discarded by the connection manager. 
+			try {
+				System.out.println(getResp.getStatusLine());
+				HeaderIterator headIterator = getResp.headerIterator();
+				while(headIterator.hasNext()) {
+					Header header = headIterator.nextHeader();
+					HeaderElement[] element = header.getElements();
+					for(HeaderElement elem : element)
+						System.out.println("key: " + elem.getName() + " value: " + elem.getValue());
+				}
+				HttpEntity getEntity = getResp.getEntity();
+				// do something useful with the response body and ensure it is fully consumed
+				EntityUtils.consume(getEntity);
+			} finally {
+				getResp.close();
+			}
+
+			HttpPost httpPost = new HttpPost("http://httpbin.org/post");
+			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+			nvps.add(new BasicNameValuePair("username", "vip"));
+			nvps.add(new BasicNameValuePair("password", "secret"));
+			httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+			CloseableHttpResponse response2 = httpclient.execute(httpPost);
+
+			try {
+				System.out.println(response2.getStatusLine());
+				HttpEntity entity2 = response2.getEntity();
+				// do something useful with the response body
+				// and ensure it is fully consumed
+				EntityUtils.consume(entity2);
+			} finally {
+				response2.close();
+			}
+		} finally {
+			httpclient.close();
+		}
+	}
+}
